0eedae576bee240092d1e6c25381c6d6c84f864a
Joel Joseph <joel.joseph211@gmail.com>
2021-10-10 13:34:14 +0530

    added save model feature

--- src/training.py
+++ src/training.py
@@ -1,7 +1,8 @@
 from utils.data_mgt import get_data
 from utils.common import read_config
-from utils.model import create_model
+from utils.model import create_model, save_model
 
+import os
 import argparse
 
 def training(config_path):
@@ -23,7 +24,15 @@ def training(config_path):
 
     history = model.fit(x_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
 
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
     
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+    
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model, model_name, model_dir_path)
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
